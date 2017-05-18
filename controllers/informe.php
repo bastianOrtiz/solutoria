d158cb9de9c9de3703ef324d3bc26e712920207a
Angelo Terrile <aterrile@gmail.com>
2017-05-18 15:11:21 -0400

    arreglo impuesto

--- controllers/informe.php
+++ controllers/informe.php
@@ -361,7 +361,6 @@ if( $_POST ){
         AND L.ano = $ano
         AND T.empresa_id = $empresa_id";
         
-        show_array($sql_impuesto);
 
         if( !@$mostrarZero )
             $sql_impuesto .= " AND L.$tipoImpuesto > 0 ";
