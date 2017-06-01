4e5a01b36e5d6586220fcb73b210e1d17eaed8a6
Angelo Terrile <aterrile@gmail.com>
2017-06-01 18:00:34 -0400

    segundo video tutorial de ayuda

--- ayuda/ayuda01.php
+++ ayuda/ayuda01.php
@@ -3,6 +3,8 @@
 <a href="index.php" class="btn_back"><i class="fa fa-chevron-left"></i> &nbsp; VOLVER </a>
 <div class="clear"></div>
 
+<h3> Cómo Recuperar mi contraseña u obtenerla por primera vez </h3>
+
 <div class="videoWrapper">
     <iframe  src="http://www.youtube.com/embed/kXM5T2SVUMU?&autoplay=1&rel=0&showinfo=0&modestbranding=1&autohide=1" frameborder="0" allowfullscreen></iframe>
 </div>
--- ayuda/index.php
+++ ayuda/index.php
@@ -3,7 +3,7 @@
 <div class="list">
     <ul>
         <li> <a href="ayuda01.php"> <i class="fa fa-asterisk"></i> Cómo Recuperar mi contraseña u obtenerla por primera vez </a> </li>
-        <li> <a href="#"> <i class="fa fa-asterisk"></i> Entendiendo el "dashboard" </a> </li>
+        <li> <a href="ayuda02.php"> <i class="fa fa-asterisk"></i> Entendiendo el "dashboard" o página de inicio</a> </li>
         <li> <a href="#"> <i class="fa fa-asterisk"></i> Entendiendo el registro de Reloj Control </a> </li>
         <li> <a href="#"> <i class="fa fa-asterisk"></i> Cómo cambiar mi contraseña </a> </li>
         <li> <a href="#"> <i class="fa fa-asterisk"></i> Cómo ver / descargar mis liquidaciones de sueldo </a> </li>
