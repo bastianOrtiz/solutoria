203c6be556e3a761b5bacde8dfabf1c0444445e5
Angelo Terrile <aterrile@gmail.com>
2017-06-07 16:54:19 -0400

    videos de autoayuda

---
+++ ayuda/ayuda03.php
@@ -0,0 +1,12 @@
+<?php include 'inc/header.php'; ?>
+
+<a href="index.php" class="btn_back"><i class="fa fa-chevron-left"></i> &nbsp; VOLVER </a>
+<div class="clear"></div>
+
+<h3>Entendiendo el registro de Reloj Control</h3>
+
+<div class="videoWrapper">
+    <iframe  src="http://www.youtube.com/embed/u6SP3-Ao8oI?&autoplay=1&rel=0&showinfo=0&modestbranding=1&autohide=1" frameborder="0" allowfullscreen></iframe>
+</div>
+
+<?php include 'inc/footer.php'; ?>
--- ayuda/index.php
+++ ayuda/index.php
@@ -4,8 +4,8 @@
     <ul>
         <li> <a href="ayuda01.php"> <i class="fa fa-asterisk"></i> Cómo Recuperar mi contraseña u obtenerla por primera vez </a> </li>
         <li> <a href="ayuda02.php"> <i class="fa fa-asterisk"></i> Entendiendo el "dashboard" o página de inicio</a> </li>
-        <li> <a href="#"> <i class="fa fa-asterisk"></i> Entendiendo el registro de Reloj Control </a> </li>
-        <li> <a href="#"> <i class="fa fa-asterisk"></i> Cómo cambiar mi contraseña </a> </li>
+        <li> <a href="ayuda03.php"> <i class="fa fa-asterisk"></i> Entendiendo el registro de Reloj Control </a> </li>
+        <li> <a href="ayuda04.php"> <i class="fa fa-asterisk"></i> Cómo cambiar mi contraseña </a> </li>
         <li> <a href="#"> <i class="fa fa-asterisk"></i> Cómo ver / descargar mis liquidaciones de sueldo </a> </li>
 
     </ul>
