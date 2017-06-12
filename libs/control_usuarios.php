0769028a6c8fc6cc3426ed101c7ad451e675dda0
Angelo Terrile <aterrile@gmail.com>
2017-06-12 17:27:34 -0400

    Corregir bug de permisos de usuarios a empresas

--- libs/control_usuarios.php
+++ libs/control_usuarios.php
@@ -32,28 +32,44 @@ if( $_SESSION[ PREFIX . 'login_admin'] != 1 ){
     $db->where('id', $_SESSION[ PREFIX . 'login_uid' ]);
     $menu_items = $db->getValue('m_usuario','roles');
     $menu_items = json_decode( $menu_items );
-    
+
     if( ( $entity != "" ) && ( $entity != "dashboard" ) && ( $entity != "login" ) ){ /** Todos pueden ver el dashboard **/
-        
         $allow_access = 0;
+
         if( $_SESSION[PREFIX.'is_trabajador'] ){
             if( !isset( $permisos_trabajador[$entity][$action] ) ){
                 $allow_access = 0;
             } else {
                 $allow_access = 1;
             }
-        } else {        
+        } else {
             if( !isset($menu_items->$entity) ){ /** Checkear Si tiene permisos para la entidad **/
                 $allow_access = 0;
             } else { /** Si tiene permiso para la entidad, ahora checkea las acciones **/
                 if( !isset( $menu_items->$entity->$action ) ){
-                    $allow_access = 0;
+                    if( $_POST['action'] == 'change_empresa' ){
+                        $empresas_user = fnListaEmpresasXUsuario($_SESSION[PREFIX . 'login_uid']);
+                        $empresas_array = array();
+                        foreach( $empresas_user as $eid ){
+                            $empresas_array[] = $eid['id_empresa'];
+                        }
+                        if( !in_array($_POST['eid'],$empresas_array) ){
+                            $allow_access = 0;
+                        } else {
+                            $allow_access = 1;
+                        }
+                    } else {
+                        $allow_access = 0;
+                    }
+
                 } else {
                     $allow_access = 1;         
                 }
             }
         }        
         
+        //show_array( $_POST );
+
         if( !$allow_access ){
             redirect(BASE_URL,'',true,'Ud. no tiene permiso para esta secci\u00f3n');
             exit();   
