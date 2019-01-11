a5b4d751be2c486a6a8ad723893bca380133a473
Unknown <aterrile@gmail.com>
2019-01-11 15:33:13 -0300

    Mostrar ID del cargo en lista de cargos

--- views/cargo/listar.php
+++ views/cargo/listar.php
@@ -42,14 +42,19 @@
                         if( $reg['cargoPadreId'] == 0 )
                             $cargo = 'Ninguno';
                         else    
-                            $cargo = fnGetNombre($reg['cargoPadreId'],'m_cargo',0);
+                            if( getComparte('Cargo') ){
+                                $cargo = fnGetNombre($reg['cargoPadreId'],'m_cargo',0);    
+                            } else {
+                                $cargo = fnGetNombre($reg['cargoPadreId'],'m_cargo',1);
+                            }
+                            
                         ?>
                             <tr>
                                 <td> <?php echo $reg['id']?> </td>
                                 <td> <?php echo $reg['nombre']?> </td>
                                 <td> <?php echo $reg['descripcion']?> </td>
                                 <td> <?php echo getTotalTrabajadoresById($reg['id']) ?> </td>
-                                <td> <?php echo "(". $reg['cargoPadreId'] .")" . $cargo ?> </td>                                
+                                <td> <?php echo "(". $reg['cargoPadreId'] .") " . $cargo ?> </td>                                
                                 <td> <?php echo fnNombreEmpresa($reg['empresa_id']) ?> </td>                                
                                 <td>
                                     <button class="btn btn-xs btn-info" data-toggle="tooltip" data-regid="<?php echo $reg['id']?>" title="Detalles"> <i class="fa fa-search"></i> </button>
