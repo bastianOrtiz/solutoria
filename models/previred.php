9bd33f4bcbedfd07db725002a2f81b2d6be35cf7
Angelo Terrile <aterrile@gmail.com>
2020-02-03 12:28:52 -0300

    Borrar antes de sobreescribir

--- models/previred.php
+++ models/previred.php
@@ -406,6 +406,8 @@ function crearTxt($post){
     }
 
     $archivo= ROOT."/private/uploads/docs/previred.txt"; // el nombre de tu archivo
+    unlink($archivo);
+    
     $empleados= $empleados; // Recibez el formulario
 
     $fch= fopen($archivo, "w+"); // Abres el archivo para escribir en él
