4d5c1fe5c0017083c9c6745e60d10c1c1be1c6de
Angelo Terrile <aterrile@tecnodatasa.cl>
2022-03-02 10:23:23 -0300

    Correccion en GROUP BY en Dashboard

--- controllers/dashboard.php
+++ controllers/dashboard.php
@@ -18,7 +18,7 @@ FROM m_departamento DEP, m_trabajador TRA
 WHERE TRA.departamento_id = DEP.id 
 AND TRA.empresa_id = " . $empresa_logged . "
 AND TRA.tipocontrato_id NOT IN (3,4)
-GROUP BY DEP.nombre
+GROUP BY DEP.nombre, DEP.id
 ";
 $deptos = $db->rawQuery( $sql );
 
